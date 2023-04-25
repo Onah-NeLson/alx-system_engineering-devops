e88bdbdf4211dad37c532af362eca9012dc74619
Onah-NeLson <Onahnelson2@gmail.com>
2023-04-24 18:06:31 -0700

    executing command

---
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -0,0 +1,9 @@
+#!/usr/bin/env puppet
+# This puppet script,creates a manifest that kills a process named killmenow.
+
+exec { 'kill':
+  command   => 'pkill -f killmenow',
+  path      => ['/usr/bin', '/usr/sbin'],
+  returns   => ['0', '1'],
+  logoutput => true,
+}
