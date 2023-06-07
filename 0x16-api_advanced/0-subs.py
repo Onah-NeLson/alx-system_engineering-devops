305ed21662e6b8c0ad9840202524801177e8240f
Onah-NeLson <Onahnelson2@gmail.com>
2023-06-06 18:14:33 -0700

    api_advanced

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
-"""a function that queries the Reddit API and returns the number of subscribers."""
+
+"""
+a function that queries the Reddit API andreturns the number of subscribers.
+"""
+
 import requests
 
 
